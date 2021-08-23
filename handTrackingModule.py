9300a8d7c3ffa0a36414f90450df1164c4cfcb5c
karishmas6 <carishmashukla28@gmail.com>
2021-08-23 12:38:15 +0530

    Hand detection module

--- handTrackingModule.py
+++ handTrackingModule.py
@@ -13,31 +13,42 @@ class HandDetector():
         self.hands = self.mpHands.Hands(self.mode, self.max_hands, self.detectionCon, self.trackCon) 
         self.mpDraw = mp.solutions.drawing_utils
 
+    def findHands(self,img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
+    def findPosition(self, img, handNum, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            oneHand = self.results.multi_hand_landmarks[handNum]
 
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            # Get information about EACH hand.
-            for id, lm in enumerate(handLms.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x*w, lm.y*h)
-                print(id, cx, cy)
-                if id == 0:
-                    cv2.circle(img, (cx,cy), 12, (255,0,255), cv2.FILLED)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            for id, lm in enumerate(oneHand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x*w, lm.y*h)
+                    lmList.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx,cy), 12, (255,0,255), cv2.FILLED)
+        return lmList
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
 
+    detector = HandDetector()
+
     while True:
         success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
         cTime = time.time()
         fps = 1/(cTime - pTime)
         pTime = cTime
@@ -48,8 +59,6 @@ def main():
             break
     cap.release()
     cv2.destroyAllWindows()
-
-
 
 if __name__ == '__main__':
     main()
