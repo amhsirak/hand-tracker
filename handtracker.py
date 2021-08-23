1fec64d070625e13f76a10311f2feef0b3ce400c
karishmas6 <carishmashukla28@gmail.com>
2021-08-23 11:47:25 +0530

    Landmark detection

--- handtracker.py
+++ handtracker.py
@@ -8,15 +8,31 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands() # using default values
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     # print(results.multi_hand_landmarks)
-    
+
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            # Get information about EACH hand.
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w, lm.y*h)
+                print(id, cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx,cy), 12, (255,0,255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 5)
 
     cv2.imshow('Image', img)
     if cv2.waitKey(1) & 0xFF == 27:
